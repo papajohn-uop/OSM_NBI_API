5b64ff7a70367e483476880c97f8287dbb14806f
Panagiotis Papaioannou <papajohn@upatras.gr>
2021-10-19 10:59:04 +0300

    NSD client creates the header now

--- client_lib/OSM_NBI_Client_nsd.py
+++ client_lib/OSM_NBI_Client_nsd.py
@@ -8,17 +8,26 @@ from  client_lib import api_helper as api_help
 
 
 class NSD:
-    def __init__(self,configuration, gen_api:api_help.GenericApi):
+    def __init__(self,configuration):
         self.configuration = configuration
+        self.main_topic=None
+        self.headers= None
+        self.topic=None
+        self.action=None
+        self.payload=None
+        self.ApiReq= None
+
+
+    def getNSDs(self):
+        gen_api=api_help.GenericApi("nsd","ns_descriptors","GET",{
+                                'Authorization': 'Bearer ' + self.configuration.token 
+                                })
         self.main_topic=gen_api.main_topic
         self.headers= gen_api.headers
         self.topic=gen_api.topic
         self.action=gen_api.action  
         self.payload=None
-        self.ApiReq= api_help.ApiRequest(configuration,self.headers)
-
-
-    def getNSDs(self):
+        self.ApiReq= api_help.ApiRequest(self.configuration,self.headers)
         if self.headers is None:
             print("Ooops")
             return None
