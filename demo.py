f366e1877c1a117e5359c4af8d2e996c26085d71
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2022-02-10 14:18:27 +0200

    Use new classes to run demo

--- demo.py
+++ demo.py
@@ -13,52 +13,16 @@ import client_lib.OSM_NBI_Client_nsd as OSM_NBI_Client_nsd
 
 def OSM_TEST():
 
-    conf_data=None
-    try:
-        with open('conf.data') as json_file:
-            conf_data = json.load(json_file)
-    except Exception as e:
-        print("Exception when trying to get conf: %s\n" % e)
-    if conf_data:
-        config=OSM_NBI_Client.Configuration(conf_data)
-    else:
-        print("No conf")
-        return
 
+    client=OSM_NBI_Client.Client("conf.data")
 
-    if (get_token(config)):
-        try:
-            # Get NSDs
+    if (client.admin.requestNewToken()):
+        NBI_nsd_client =OSM_NBI_Client_nsd.NSD(client.conf)
+        #response = NBI_nsd_client.getNSDs()
+        response = client.nsd.getNSDs()
+        for resp in response:
+            pprint(resp["name"])
 
-            NBI_nsd_client =OSM_NBI_Client_nsd.NSD(config)
-
-            response = NBI_nsd_client.getNSDs()
-        
-            for resp in response:
-                pprint(resp["name"])
-        except Exception as e:
-            print("Exception when calling NSD->getNSDs: %s\n" % e)
-    else:
-        print("FAIL.......")
-
-
-
-
-
-def get_token(config):
-   
-
-    NBI_admin_client =OSM_NBI_Client_admin.Admin(config)
-
-
-    try:
-        # Request a new Token
-        response = NBI_admin_client.requestNewToken()
-        config.token=response["id"]
-    except Exception as e:
-        print("Exception when calling Admin->requestNewToken: %s\n" % e)
-        return False
-    return True
 
 
 OSM_TEST()
