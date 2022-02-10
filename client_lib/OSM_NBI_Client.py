c0c52a6f102d26e5f15a2511dd7acff183141043
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2022-02-10 10:49:39 +0200

    Use conf file to keep login info for OSM

--- client_lib/OSM_NBI_Client.py
+++ client_lib/OSM_NBI_Client.py
@@ -3,80 +3,91 @@ import http.client
 import json
 import yaml
 
-from  client_lib import OSM_NBI_Client_admin as admin_client
-from  client_lib import OSM_NBI_Client_nsd as nsd_client
-from  client_lib import api_helper as api_help 
-
 class Configuration:
-    def __init__(self):
-        self.host = "xx.xx.xx.xx"
+    def __init__(self,conf):
+        self.host = conf["host"]
         # Username for HTTP basic authentication
-        self.username = "username"
+        self.username = conf["username"]
         # Password for HTTP basic authentication
-        self.password = "password"
+        self.password = conf["password"]
         #Version
-        self.version="v1"
+        self.version=conf["version"]
         #authorization token
         self.token=None
 
-# class GenericApi():
-#     def __init__(self,main_topic,topic,action,headers):
-#         self.main_topic=main_topic
-#         self.topic=topic
-#         self.action=action
-#         self.headers=headers
-#         self.payload=None
+class GenericApi():
+    def __init__(self,main_topic,topic,action,headers):
+        self.main_topic=main_topic
+        self.topic=topic
+        self.action=action
+        self.headers=headers
+        self.payload=None
 
 
 
-# class ApiRequest():
-#     def __init__(self,configuration,headers):
-#         self.configuration=configuration
-#         self.conn=http.client.HTTPConnection(configuration.host)
-#         self.headers=headers
+class ApiRequest():
+    def __init__(self,configuration,headers):
+        self.configuration=configuration
+        self.conn=http.client.HTTPConnection(configuration.host)
+        self.headers=headers
        
-#     def send_request(self,action,main_topic, topic,payload):
-#         self.conn.request(action, "/osm/"+main_topic+"/"+self.configuration.version+"/" + topic, payload, self.headers)
-#         res = self.conn.getresponse()
-#         data = res.read()
+    def send_request(self,action,main_topic, topic,payload):
+        self.conn.request(action, "/osm/"+main_topic+"/"+self.configuration.version+"/" + topic, payload, self.headers)
+        res = self.conn.getresponse()
+        data = res.read()
 
-#         try:
-#             resp_yaml=yaml.safe_load(data)
-
-#             return resp_yaml
-#         except yaml.YAMLError as exc:
-#             print(exc)
-#             return None
-
+        try:
+            resp_yaml=yaml.safe_load(data)
+            return resp_yaml
+        except yaml.YAMLError as exc:
+            print(exc)
+            return None
 
 
+class Admin:
+    def __init__(self,configuration, gen_api:GenericApi):
+        self.configuration = configuration
+        self.main_topic=gen_api.main_topic
+        self.headers= gen_api.headers
+        self.topic=gen_api.topic
+        self.action=gen_api.action  
+        self.payload=None
+        self.ApiReq= ApiRequest(configuration,self.headers)
 
-# class NSD:
-#     def __init__(self,configuration, gen_api:GenericApi):
-#         self.configuration = configuration
-#         self.main_topic=gen_api.main_topic
-#         self.headers= gen_api.headers
-#         self.topic=gen_api.topic
-#         self.action=gen_api.action  
-#         self.payload=None
-#         self.ApiReq= ApiRequest(configuration,self.headers)
-
-
-#     def getNSDs(self):
-#         if self.headers is None:
-#             print("Ooops")
-#             return None
-#         if self.configuration.token is None:
-#             print("No authorzation")
-#             return None
-#         pprint("Get NSDS")
-#         self.payload=""
-#         self.topic="ns_descriptors"
-
-#         return (self.ApiReq.send_request("GET",self.main_topic,self.topic,self.payload) )
+    
+    def requestNewToken(self):
+        #TODO
+        if self.headers is None:
+            print("Ooops")
+            return None
+        pprint("Request New Token")
+        self.payload="{\"username\": \""+ self.configuration.username+"\",  \"password\": \""+self.configuration.password+"\"}"
+        return (self.ApiReq.send_request("POST",self.main_topic,self.topic,self.payload) )
 
 
 
+class NSD:
+    def __init__(self,configuration, gen_api:GenericApi):
+        self.configuration = configuration
+        self.main_topic=gen_api.main_topic
+        self.headers= gen_api.headers
+        self.topic=gen_api.topic
+        self.action=gen_api.action  
+        self.payload=None
+        self.ApiReq= ApiRequest(configuration,self.headers)
 
+
+    def getNSDs(self):
+        if self.headers is None:
+            print("Ooops")
+            return None
+        if self.configuration.token is None:
+            print("No authorzation")
+            return None
+        pprint("Get NSDS")
+        self.payload=""
+        self.topic="ns_descriptors"
+
+        return (self.ApiReq.send_request("GET",self.main_topic,self.topic,self.payload) )
 
 
--- demo.py
+++ demo.py
@@ -8,47 +8,59 @@ import client_lib.OSM_NBI_Client as OSM_NBI_Client
 
 
 
-config=OSM_NBI_Client.Configuration()
-gen_API=OSM_NBI_Client.api_help.GenericApi("admin","tokens","POST",{
+def OSM_TEST():
+
+    conf_data=None
+    try:
+        with open('conf.data') as json_file:
+            conf_data = json.load(json_file)
+    except Exception as e:
+        print("Exception when trying to get conf: %s\n" % e)
+    if conf_data:
+        config=OSM_NBI_Client.Configuration(conf_data)
+    else:
+        print("No conf")
+        return
+
+
+    if (get_token(config)):
+        try:
+            # Get NSDs
+            gen_API=OSM_NBI_Client.GenericApi("nsd","ns_descriptors","GET",{
+                                        'Authorization': 'Bearer ' + config.token 
+                                        })
+            NBI_nsd_client =OSM_NBI_Client.NSD(config,gen_API)
+
+            response = NBI_nsd_client.getNSDs()
+        
+            for resp in response:
+                pprint(resp["name"])
+        except Exception as e:
+            print("Exception when calling NSD->getNSDs: %s\n" % e)
+    else:
+        print("FAIL.......")
+
+
+
+
+
+def get_token(config):
+   
+    gen_API=OSM_NBI_Client.GenericApi("admin","tokens","POST",{
         'Content-Type': 'application/json'
         })
-NBI_admin_client =OSM_NBI_Client.admin_client.Admin(config)
+    NBI_admin_client =OSM_NBI_Client.Admin(config,gen_API)
 
 
-try:
-    # Request a new Token
-    response = NBI_admin_client.requestNewToken()
-    print(response)
-    print(response["id"])
-    config.token=response["id"]
-    print(config.token)
-except Exception as e:
-    print("Exception when calling Admin->requestNewToken: %s\n" % e)
-
-try:
-    # Get NSDs
-    NBI_nsd_client =OSM_NBI_Client.nsd_client.NSD(config)
-
-    response = NBI_nsd_client.getNSDs()
-    # print(response)
-    # print("*************")
-    # print(type(response))
-    # print("*************")
-    # pprint(response[0])
-    # print("*************")
-    # pprint(response[0]["description"])
-    # print("*************")
-    # pprint(response[0]["id"])
-    # print("*************")
-    # pprint(response[0]["name"])
-    pprint(len(response))
-    for resp in response:
-        pprint("***************")
-        pprint(resp)
-        pprint("***************")
-        pprint(resp["description"])
-except Exception as e:
-    print("Exception when calling NSD->getNSDs: %s\n" % e)
+    try:
+        # Request a new Token
+        response = NBI_admin_client.requestNewToken()
+        config.token=response["id"]
+    except Exception as e:
+        print("Exception when calling Admin->requestNewToken: %s\n" % e)
+        return False
+    return True
 
 
+OSM_TEST()
 
