6d707c0dc62789af35783cb485bf10cf9397cb7b
Panagiotis Papaioannou <65160692+papajohn-uop@users.noreply.github.com>
2022-02-10 13:09:25 +0200

    Add client class and use conf into this

--- client_lib/OSM_NBI_Client.py
+++ client_lib/OSM_NBI_Client.py
@@ -6,8 +6,8 @@ import yaml
 from  client_lib import OSM_NBI_Client_admin as admin 
 
 
-
 class Configuration:
+    
     def __init__(self,conf):
         self.host = conf["host"]
         # Username for HTTP basic authentication
@@ -18,8 +18,31 @@ class Configuration:
         self.version=conf["version"]
         #authorization token
         self.token=None
+        self.admin=None
 
 
 
 
 
+class Client:
+    def __init__(self,conf:Configuration,confFile):
+        self.confd = conf
+        self.conf=self.__readConf__(confFile) 
+        self.admin=admin.Admin(self.conf)
+
+    
+    def __readConf__(self,confFile):
+        print("READ")
+        conf_data=None
+        config=None
+        try:
+            with open('conf.data') as json_file:
+                conf_data = json.load(json_file)
+        except Exception as e:
+            print("Exception when trying to get conf: %s\n" % e)
+        if conf_data:
+            config=Configuration(conf_data)
+        else:
+            print("No conf")
+        return config
+            
